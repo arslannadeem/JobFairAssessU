a0f4f0460d177a848a92eb8c9bfb5138ae3cf350
arslannadeem <i141841@nu.edu.pk>
2017-12-20 17:07:44 +0500

    Evaluate Quiz

--- angular-src/src/app/components/result/result.component.ts
+++ angular-src/src/app/components/result/result.component.ts
@@ -21,18 +21,39 @@ export class ResultComponent implements OnInit {
   }
 
   ResultsHighlights() {
-    console.log("script");
+     console.log("script");
     let i = 0;
     for (var j = 0; j < this.data.length; j++) {
-      var question = document.getElementById('+ (100+j) +');
-      console.log(question)
-      // for(var k=0;k<choice.length;k++)
-      // {
-      //   if(choice[k].Type)
-      //   {
-      //      console.log(k);
-      //   }
-      // }
+      var question = document.getElementById(String(100+j));
+      var choice = question.getElementsByTagName("div");
+      
+      let index = 0;
+      for(var k=0;k<choice.length;k++)
+      {
+        if(this.data[j].Choices[k].Status)
+        {
+          index = k;
+          k=choice.length;
+        }
+      }
+
+        if(this.choices[j] == index)
+        {
+          //choice[k].getAttribute("id")
+          console.log("Question true");
+          index=0;
+          var statement = document.getElementById(String(100+j));
+          statement.getElementsByClassName(String(j))[index].setAttribute("class", "correct");
+        }
+        else
+        {
+          console.log("Question false");
+          var statement = document.getElementById(String(100+j));
+          statement.getElementsByClassName(String(j))[this.choices[j]].setAttribute("class", "wrong");
+        
+          var statement1 = document.getElementById(String(100+j));
+          statement1.getElementsByClassName(String(j))[index].setAttribute("class", "correct_with_wrong");
+        }
+      }
     }
-  }
 }
