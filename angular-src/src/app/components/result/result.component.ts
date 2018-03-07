ecb4c133dab23bf9d1b428985b142598cabae09d
arslannadeem <i141841@nu.edu.pk>
2018-03-07 15:10:24 +0500

    Article Display V1

--- angular-src/src/app/components/result/result.component.ts
+++ angular-src/src/app/components/result/result.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { QuizService } from '../../services/quiz.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-result',
@@ -15,7 +16,11 @@ export class ResultComponent implements OnInit {
 
   wrong_question = [];
 
-  constructor(private router: Router,private quizService : QuizService) { }
+  constructor(private router: Router,private quizService : QuizService, private flashMessgae: FlashMessagesService) { }
+
+  final_list_of_articles = [];
+  View_result_array = [];
+  view = false;
 
   ngOnInit() {
     this.questions = JSON.parse(localStorage.getItem("question"));
@@ -46,7 +51,6 @@ export class ResultComponent implements OnInit {
       }
       else {
         console.log("Question false");
-        console.log(this.questions[j]);
 
         if (index != -1) {
 
@@ -62,8 +66,6 @@ export class ResultComponent implements OnInit {
       }
     }
 
-    //console.log(this.wrong_question);
-
     var select_question = [];
 
     for(var i=0;i<this.wrong_question.length;i++)
@@ -71,10 +73,97 @@ export class ResultComponent implements OnInit {
         select_question[i] = this.data[this.wrong_question[i]];
     }
 
-    console.log(select_question);
+    if(this.wrong_question.length>0)
+    {
+      this.quizService.getArticleByWrongQuestion(select_question).subscribe(data => {
+        if (data.success) {
 
+          var list = data.articles;
 
+          // ----- if duplicate articles then remove it --------
+
+          for(var i=0;i<list.length-1;i++)
+          {
+            for(var j=i+1;j<list.length;j++)
+            {
+              if(list[j]._id!=-1 && list[i]._id!=-1 && list[i]._id==list[j]._id)
+              {
+                list[j]._id=-1;
+              }
+            }
+          }
+
+          // ----------- save it other array -------
+          
+          for(var i=0,j=0;i<list.length-1;i++)
+          {
+            if(list[i]._id!=-1)
+            {
+              this.final_list_of_articles[j]=list[i];
+              j++;
+            }
+          }
+
+          console.log("articles : ",this.final_list_of_articles);
+
+          this.flashMessgae.show("Topcis Shows", { cssClass: 'alert-success', timeout: 3000 });
+        }
+        else {
+          this.flashMessgae.show("Something went Wrong", { cssClass: 'alert-danger', timeout: 3000 });
+        }
+      });
+    }
   }
 
+  ShowArticles()
+  {
+    console.log("call");
+      this.view = true;
+
+      // ----- if duplicate articles then remove it --------
+
+      var temp_question_no = [];
+      var temp_list = this.wrong_question;
+      
+      for(var i=0;i<temp_list.length-1;i++)
+      {
+        for(var j=i+1;j<temp_list.length;j++)
+        {
+          if(temp_list[j]!=-1 && temp_list[i]!=-1 && temp_list[i]==temp_list[j])
+          {
+            temp_list[j]=-1;
+          }
+        }
+      }
+
+      // ----------- save it other array -------
+          
+      for(var i=0,j=0;i<temp_list.length-1;i++)
+      {
+        if(temp_list[i]!=-1)
+        {
+          temp_question_no[j]=temp_list[i];
+          j++;
+        }
+      }
+
+      console.log("question number list : " , temp_question_no);
+      //---------------------
+
+      for(var i=0,k=0;i<temp_question_no.length;i++)
+      {
+          for(var j=0;j<this.final_list_of_articles.length;j++)
+          {
+            if(this.data[temp_question_no[i]].Sub_Topic_Name == this.final_list_of_articles[j].Sub_Topic_Name
+            && this.data[temp_question_no[i]].Topic_Name == this.final_list_of_articles[j].Topic_Name)
+            {
+              this.View_result_array[k]={"Question":this.data[this.questions[i]].Description,"Description": String(this.final_list_of_articles[j].Description) };
+              k++;
+            }
+          }
+      }
+
+      console.log(this.View_result_array);
+  }
 
 }
--- models/question.js
+++ models/question.js
@@ -24,67 +24,54 @@ module.exports.addQuestion = function (newQuestion, callback) {
 
     var temp_array = [];
 
-    console.log(newQuestion);
-    
-    for(var i=0;i<newQuestion.Choice_ID.length;i++)
-    {
+    for (var i = 0; i < newQuestion.Choice_ID.length; i++) {
         var temp_Choices = {
             _id: Number,
             Status: false,
-            Description : String,
+            Description: String,
         };
 
-        temp_Choices._id = i+1;
+        temp_Choices._id = i + 1;
         temp_Choices.Description = newQuestion.Choice_Text[i];
 
         temp_array.push(temp_Choices);
     }
 
-    if(newQuestion.Choice_Check)
-    {
-        for(var i=0;i<newQuestion.Choice_Check.length;i++)
-        {
-            temp_array[newQuestion.Choice_Check[i]-1].Status=true;
+    if (newQuestion.Choice_Check) {
+        for (var i = 0; i < newQuestion.Choice_Check.length; i++) {
+            temp_array[newQuestion.Choice_Check[i] - 1].Status = true;
         }
     }
 
     var temp_level = [];
 
-    for(var i=0;i<newQuestion.Level.length;i++)
-    {
-        temp_level[i]=newQuestion.Level[i].itemName;
+    for (var i = 0; i < newQuestion.Level.length; i++) {
+        temp_level[i] = newQuestion.Level[i].itemName;
     }
 
     var temp_sub_topic = [];
 
-    for(var i=0;i<newQuestion.SubTopics.length;i++)
-    {
-        temp_sub_topic[i] = newQuestion.SubTopics[i].itemName.replace(/^"(.*)"$/, '$1');; 
+    for (var i = 0; i < newQuestion.SubTopics.length; i++) {
+        temp_sub_topic[i] = newQuestion.SubTopics[i].itemName.replace(/^"(.*)"$/, '$1');;
     }
     var id;
-    Question.count({}, function( err, count){
+    Question.count({}, function (err, count) {
         console.log(count);
-        if(!count>0)
-        {
+        if (!count > 0) {
             id = 1;
         }
-        else
-        {
+        else {
             id = ++count;
         }
 
         let obj = new Question({
-            _id : id,
-            Description : newQuestion.Question,
-            Level : temp_level,
-            Topic_Name : newQuestion.Topics[0].itemName,
-            Sub_Topic_Name : temp_sub_topic,
-            Choices : temp_array
+            _id: id,
+            Description: newQuestion.Question,
+            Level: temp_level,
+            Topic_Name: newQuestion.Topics[0].itemName,
+            Sub_Topic_Name: temp_sub_topic,
+            Choices: temp_array
         });
-
-        console.log(obj);
-        // ----- call question add ----
-        //Question.addQuestion(obj);
 
         obj.save(callback);
     })
@@ -93,5 +80,5 @@ module.exports.addQuestion = function (newQuestion, callback) {
 module.exports.get_Questions_By_Topic_Name = function (data, callback) {
     console.log("question model")
     console.log(data);
-    Question.find({ 'Topic_Name': "\n" + data.topic + "\n" },callback);
+    Question.find({ 'Topic_Name': "\n" + data.topic + "\n" }, callback);
 }
